0052f56a63d159da91180b86816a43398fcc5aed
Adel <adelremili@gmail.com>
2025-12-12 11:39:15 +0100

    add logout endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenBlacklistView,
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from .views import SpectatorRegistrationView
 
@@ -7,4 +11,5 @@ urlpatterns = [
     path("register/", SpectatorRegistrationView.as_view(), name="spectator-register"),
     path("token/", TokenObtainPairView.as_view(), name="token-obtain"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("logout/", TokenBlacklistView.as_view(), name="logout"),
 ]
