0f28322b8c825d2bc0568c264ef378f444863d35
Adel <adelremili@gmail.com>
2025-12-12 10:10:40 +0100

    add  author delete endpoint

--- movies/views.py
+++ movies/views.py
@@ -1,15 +1,24 @@
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import AllowAny
 
 from .models import Author
 from .serializers import AuthorSerializer
 
 
-class AuthorViewSet(viewsets.ReadOnlyModelViewSet):
+class AuthorViewSet(viewsets.ModelViewSet):
     """
-    API for viewing authors.
+    API to manage authors.
     """
+    http_method_names = ["get", "put", "patch", "delete"]
 
     queryset = Author.objects.prefetch_related("movies").all()
     serializer_class = AuthorSerializer
     permission_classes = [AllowAny]
+
+    def perform_destroy(self, instance):
+        if instance.movies.exists():
+            raise ValidationError(
+                {"detail": "Cannot delete author with linked movies."}
+            )
+        instance.delete()
